67f3b40054e80bc1143f4f0197dfec68ea207c42
muhammad rio <muhammad.rioperm@gmail.com>
2023-04-20 18:10:28 +0700

    add certificate pinner

--- core/src/main/java/com/riopermana/core/data/network/di/NetworkModule.kt
+++ core/src/main/java/com/riopermana/core/data/network/di/NetworkModule.kt
@@ -10,6 +10,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
@@ -30,6 +31,13 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideOkhttpClient(): OkHttpClient = OkHttpClient.Builder().apply {
+        val hostname = BuildConfig.BASE_URL.substringAfter("//")
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/1UPHAdcUbUoOcd5rDTD/0oMSnngCU6YzXzpByO4CCp4=")
+            .add(hostname, "sha256/RQeZkB42znUfsDIIFWIRiYEcKl7nHwNFwWCrnMMJbVc=")
+            .add(hostname, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+            .add(hostname, "sha256/e0IRz5Tio3GA1Xs4fUVWmH1xHDiH2dMbVtCBSkOIdqM=")
+            .build()
         val httpLoggingInterceptor = if (BuildConfig.DEBUG) {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         } else {
@@ -37,6 +45,7 @@ object NetworkModule {
         }
         addInterceptor(httpLoggingInterceptor)
         addInterceptor(GithubRepoApiInterceptor())
+        certificatePinner(certificatePinner)
     }.build()
 
     @Provides
